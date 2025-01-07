eebcb229e476f2db3959c48741a89c58acd8bba9
Rodrigo-Brezolin-Buquera <rodrigobbuquera@gmail.com>
2025-01-07 14:45:39 -0300

    lógica

--- src/App.js
+++ src/App.js
@@ -2,17 +2,54 @@ import { useState } from "react";
 import routines from "./routines";
 
 const App = () => {
-  const today = new Date().getDay()
-  const [weekday, setWeekday] = useState(today)
-  const [view, setView] = useState("gym")
+  const today = new Date().getDay();
+  const [weekday, setWeekday] = useState(today);
+  const [view, setView] = useState("gym");
+  const routine = routines[weekday];
 
-  console.log(routines[weekday].day)
+  const asanas = routine.asanas.map((asana) => {
+    return <p key={asana}> {asana}</p>;
+  });
+
+  const TrainingSection = () => {
+    return (
+      <section onClick={()=> setView("asanas")} >
+        <h1>{routine.day}</h1>
+        <h2>Cardio:</h2>
+        <p>{routine.cardio}</p>
+        <h2>Treino:</h2>
+        <p>{routine.routine}</p>
+        <h2>Adiconal:</h2>
+        <p>{routine.extra}</p>
+      </section>
+    );
+  };
+
+  const AsanasSection = () => {
+    return (
+      <section onClick={()=> setView("gym")}>
+        <h2>Asanas:</h2>
+        {asanas}
+      </section>
+    );
+  };
+
+  const ViewSelector = () => {
+    switch (view) {
+      case "gym":
+        return <TrainingSection />;
+      case "asanas":
+        return <AsanasSection />;
+      default:
+        return <TrainingSection />;
+    }
+  };
 
   return (
-    <div >
-        aaaaaa
-    </div>
+    <main>
+      <ViewSelector />
+    </main>
   );
-}
+};
 
 export default App;
